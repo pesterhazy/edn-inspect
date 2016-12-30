f8c3adc00936394a6f1f807397747fa249fb7b70
Paulus Esterhazy <pesterhazy@gmail.com>
2016-12-30 09:52:10 +0100

    Init

---
+++ src/cljs/efrisk/app.cljs
@@ -0,0 +1,16 @@
+(ns efrisk.app
+  (:require [reagent.core :as reagent :refer [atom]]
+            [cljs.reader :refer [read-string]]
+            [datafrisk.core :as d]))
+
+(defonce state (reagent/atom nil))
+
+(defn frisk-view []
+  (let [data (some-> (js/document.getElementById "edn-data")
+                     .-text
+                     read-string)]
+    [d/Root data 0 state]))
+
+(defn init []
+  (reagent/render-component (fn [] [frisk-view])
+                            (.getElementById js/document "container")))
